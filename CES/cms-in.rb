545d23374dacf1c5012ab3eed36a57817fc5c533
David Schanen <ds2222@att.com>
2012-12-20 00:01:30 -0800

    update

--- CES/cms-in.rb
+++ CES/cms-in.rb
@@ -1,2 +1,8 @@
-say "Hello, World."
+choices = IO.readlines("first_names")
+ask "Hello, World. Who are you trying to reach?", {
+    :choices => choices
+    :onChoice => lambda { |event|
+        say("You said your" + event.value + "is" + event.choice.interpretation)
+    }
+}
 hangup
