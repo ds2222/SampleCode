20d072dcd806628de13e3d60029c4f383d1f5ffa
David Schanen <ds2222@att.com>
2012-12-20 00:50:49 -0800

    update

--- CES/cms-out.rb
+++ CES/cms-out.rb
@@ -1,6 +1,4 @@
-#call ('sip:mtv969@sip2sip.info'), {
-#call ('+14256287318'), {
-call ('+12069549666'), {
+call ('sip:mtv969@sip2sip.info'), {
    :onAnswer => lambda {
        say "Hello, World"
    }
