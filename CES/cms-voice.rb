d95a64694a136fcf41143a2d96e9ab5592958113
David Schanen <ds2222@att.com>
2012-12-20 09:36:02 -0800

    update

--- CES/cms-voice.rb
+++ CES/cms-voice.rb
@@ -1,7 +1,7 @@
-ask "Hello, World. Say a number between one and ten.", {
+ask "Hello World, Please say a number between one and ten.", {
   :choices => "number(one, two, three, four, five, six, seven, eight, nine, ten)",
   :onChoice => lambda { |event|
-    say("The " + event.choice.interpretation + " you chose is " + event.value + ". Thank you, Goodbye")   
+    say("The " + event.value + " you chose is " + event.choice.interpretation + ". Thank you, Goodbye")   
   }
 }
 hangup
