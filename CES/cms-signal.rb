fd0948498eff22d53b0b5eac3af8fe6c4a8a4d09
David Schanen <ds2222@att.com>
2012-12-20 01:52:56 -0800

    update

--- CES/cms-signal.rb
+++ CES/cms-signal.rb
@@ -2,7 +2,7 @@ call ('sip:mtv969@sip2sip.info'), {
   :allowSignals => "exit",
   :onAnswer => lambda {
     say "Hello World, please hold"
-    say "https://raw.github.com/ds2222/SampleCode/CES/master/music.mp3"
+    say "https://raw.github.com/ds2222/SampleCode/master/CES/music.mp3"
     say "Thanks for holding. Goodbye"
   }
 }
