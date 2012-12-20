d7dfcd677624d096d1945993997cb7f9eb1d10d4
David Schanen <ds2222@att.com>
2012-12-20 00:36:36 -0800

    update

--- CES/cms-in.rb
+++ CES/cms-in.rb
@@ -1,7 +1,2 @@
-ask "Hello, World. Who are you trying to reach?", {
-    :choices => "name(david,john)",
-    :onChoice => lambda { |event|
-        say("You said your" + event.value + "is" + event.choice.interpretation)
-    }
-}
+say "Hello, World."
 hangup
